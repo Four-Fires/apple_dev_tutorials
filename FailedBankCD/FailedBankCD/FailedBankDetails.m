d18f39e8f899a17d86b821c25146bea02211d303
matt <matthewatgg@gmail.com>
2013-12-15 19:28:05 -0500

    finished part 1.

---
+++ FailedBankCD/FailedBankCD/FailedBankDetails.m
@@ -0,0 +1,20 @@
+//
+//  FailedBankDetails.m
+//  FailedBankCD
+//
+//  Created by matthew on 2013-12-15.
+//  Copyright (c) 2013 matthew. All rights reserved.
+//
+
+#import "FailedBankDetails.h"
+#import "FailedBankInfo.h"
+
+
+@implementation FailedBankDetails
+
+@dynamic closeDate;
+@dynamic updateDate;
+@dynamic zip;
+@dynamic info;
+
+@end
---
+++ FailedBankCD/FailedBankCD/FailedBankInfo.m
@@ -0,0 +1,19 @@
+//
+//  FailedBankInfo.m
+//  FailedBankCD
+//
+//  Created by matthew on 2013-12-15.
+//  Copyright (c) 2013 matthew. All rights reserved.
+//
+
+#import "FailedBankInfo.h"
+
+
+@implementation FailedBankInfo
+
+@dynamic name;
+@dynamic city;
+@dynamic state;
+@dynamic details;
+
+@end
