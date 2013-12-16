a3315b041a98aa54257c7699628187a65a2bd46c
matt <matthewatgg@gmail.com>
2013-12-15 22:28:42 -0500

    finished part 2

---
+++ CoreDataTutorial2/CoreDataTutorial2/FailedBankDetails.h
@@ -0,0 +1,21 @@
+//
+//  FailedBankDetails.h
+//  FailedBankCD
+//
+//  Created by matthew on 2013-12-15.
+//  Copyright (c) 2013 matthew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class FailedBankInfo;
+
+@interface FailedBankDetails : NSManagedObject
+
+@property (nonatomic, retain) NSDate * closeDate;
+@property (nonatomic, retain) NSDate * updateDate;
+@property (nonatomic, retain) NSNumber * zip;
+@property (nonatomic, retain) FailedBankInfo *info;
+
+@end
---
+++ CoreDataTutorial2/CoreDataTutorial2/FailedBankInfo.h
@@ -0,0 +1,21 @@
+//
+//  FailedBankInfo.h
+//  FailedBankCD
+//
+//  Created by matthew on 2013-12-15.
+//  Copyright (c) 2013 matthew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class FailedBankDetails;
+
+@interface FailedBankInfo : NSManagedObject
+
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSString * city;
+@property (nonatomic, retain) NSString * state;
+@property (nonatomic, retain) FailedBankDetails *details;
+
+@end
